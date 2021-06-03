55f90ceba05fe64d23fdec9c925fed8d57af983b
Amaro Basualdo <basualdo.amaro.luciano@gmail.com>
2021-06-03 13:19:04 -0300

    CP commiteado por basualdo.amaro.luciano@gmail.com a las 1622737144512

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -182,10 +182,8 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for(var i = 0; i < 60; i++){
-    if(i % 6 === 0) {
-      return i;
-    }
+  for(var i = 0; i < 11; i++){
+    return i*6;
   }
 }
 
